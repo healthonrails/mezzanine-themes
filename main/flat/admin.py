c95ac4956388f6c04132ca4b4a4b9d1607bbd918
Chen Yang <healthonrails@gmail.com>
2019-07-25 12:57:07 -0400

    Register new models to Django admin

--- main/flat/admin.py
+++ main/flat/admin.py
@@ -1,4 +1,35 @@
 from django.contrib import admin
 
+# Register your models here.
+from mezzanine.core.admin import TabularDynamicInlineAdmin 
+from mezzanine.pages.admin import PageAdmin
+# Register your models here.
+from .models import (HomePage,IconBlurb,
+                    Slide,Service,Program,
+                    AsideLink,AsidePage)
+
+class SlideInline(TabularDynamicInlineAdmin):
+    model = Slide
+    
+
+class IconBlurbline(TabularDynamicInlineAdmin):
+    model = IconBlurb
+    
+class ServiceInline(TabularDynamicInlineAdmin):
+    model = Service
+    
+class ProgramInline(TabularDynamicInlineAdmin):
+    model = Program
+    
+class AsideLinkInline(TabularDynamicInlineAdmin):
+    model = AsideLink
+    
+class AsidePageAdmin(PageAdmin):
+    inlines = (AsideLinkInline,)
+
+class HomePageAdmin(PageAdmin):
+    inlines = (SlideInline,ServiceInline,IconBlurbline,ProgramInline)
 
 
+admin.site.register(HomePage, HomePageAdmin)
+admin.site.register(AsidePage,AsidePageAdmin)
